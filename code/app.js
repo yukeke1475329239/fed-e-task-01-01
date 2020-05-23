cc103d88302b7c67e6baa50bf80a0d2d1eb0e388
keke.yu <keke.yu@17zuoye.com>
2020-05-23 23:39:46 +0800

    模块一作业

--- code/app.js
+++ code/app.js
@@ -1,1 +1,70 @@
-// 示例作业提交
+
+// 第一题
+// var a = []
+// for(var i=0;i<10;i++){
+//   a[i] = function (){
+//     console.log(i) //打印10 
+//   }
+// }
+// a[6]()
+//第二题
+// var temp = 123
+
+// import { resolve } from "dns";
+// import { rejects } from "assert";
+
+// if(true){
+//   console.log(temp)
+//   let temp
+// }
+
+//第三题
+
+// var arr = [12,34,32,89,4]
+// console.log(Math.min(...arr))
+
+//第五题
+
+// var a = 10;
+
+// var obj = {
+//   a:20,
+//   fn(){
+//     setTimeout(()=>{
+//      console.log(this.a)
+//     },)
+//   }
+// }
+// obj.fn()
+
+//第九题
+
+// setTimeout(function() {
+//   var a = "hello"
+//   setTimeout(function() {
+//     var b = "lagou"
+//     setTimeout(function() {
+//       var c = "I ❤ U"
+//       console.log(a+b+c)
+//     }, 10);
+//   }, 10);
+// }, 10);
+
+// const promise = new Promise((resolve,reject)=>{
+//   var a = "hello"
+//   resolve(a)
+// }).then((value,)=>{
+//   new Promise((resolve,reject)=>{
+//     var b = "lagou"
+//     resolve(value+b)
+//   }).then(val=>{
+//     var c = "I ❤ U"
+//     console.log(val+c)
+//   })
+// })
+
+
+
+
+
+
